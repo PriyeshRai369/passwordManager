830634b9bcc180e5c4e9287f549ea9c7c30d83a3
Priyesh Rai <105197078+PriyeshRai369@users.noreply.github.com>
2024-05-22 12:18:37 +0530

    Update Heading.jsx

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -4,6 +4,17 @@ import style from './Heading.module.css';
 function Heading() {
   return (
     <h3 className={style.heading}>&lt;Secure <span>Nest</span>/&gt;</h3>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3485555502925175"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3485555502925175"
+     data-ad-slot="1197850142"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
   )
 }
 
